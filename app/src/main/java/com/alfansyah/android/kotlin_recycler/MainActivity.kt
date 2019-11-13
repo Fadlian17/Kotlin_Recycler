2552b390ebe96686a381a5f29d014dfdfc4487c0
Fadlian17 <alfansyahfadlian@gmail.com>
2019-11-13 11:06:06 +0700

    update Recycler v.3

--- app/src/main/java/com/alfansyah/android/kotlin_recycler/MainActivity.kt
+++ app/src/main/java/com/alfansyah/android/kotlin_recycler/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +13,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var rvFootballs: RecyclerView
     private var list:ArrayList<Football> = arrayListOf()
+    private var title: String = "Mode List"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBarTitle(title)
 
         rvFootballs = findViewById(R.id.rv_footballs)
         rvFootballs.setHasFixedSize(true)
@@ -44,19 +47,36 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
+                title = "Mode List"
                 showRecyclerList()
             }
             R.id.action_grid -> {
+                title = "Mode Grid"
                 showRecyclerGrid()
             }
             R.id.action_cardview -> {
+                title = "Mode Card"
+                showRecyclerCardView()
             }
         }
+        setActionBarTitle(title)
     }
 
     private fun showRecyclerGrid() {
         rvFootballs.layoutManager = GridLayoutManager(this, 2)
         val gridFootballAdapter = GridFootballAdapter(list)
         rvFootballs.adapter = gridFootballAdapter
+    }
+
+    private fun showRecyclerCardView() {
+        rvFootballs.layoutManager = LinearLayoutManager(this)
+        val cardViewFootballAdapter = CardViewFootballAdapter(list)
+        rvFootballs.adapter = cardViewFootballAdapter
+    }
+
+    private fun setActionBarTitle(title: String) {
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).title = title
+        }
     }
 }
